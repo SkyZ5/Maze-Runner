026198bc963b6dc9b791870d1b5fbdb5556536be
SkyZ5 <145574808+SkyZ5@users.noreply.github.com>
2024-05-31 09:29:51 -0400

    5/31/2024
    Added the zombie health and got them to take damage and die and disappear. 
    Also made them respawn after 5 seconds.

--- main.py
+++ main.py
@@ -57,6 +57,8 @@ filter.fill(pygame.color.Color('Grey'))
 distance_back = 50
 z1_health = 100
 z2_health = 100
+z1_dead = False
+z2_dead = False
 
 # Spritesheets
 player_sheet_image = pygame.image.load("player_sheet.png").convert_alpha()
@@ -99,6 +101,7 @@ clock = pygame.time.Clock()
 # -------- Main Program Loop -----------
 while run:
     clock.tick(165)
+
     # Movement
     keys = pygame.key.get_pressed()  # checking pressed keys
 
@@ -175,41 +178,62 @@ while run:
         z2.init_y += 1
     
     # Mob Movement
-    z1_action = z1.move_towards_player(p)
-    z2_action = z2.move_towards_player(p)
 
-    if z1_mask.overlap(walls_mask, (pos[0] - z1.x, pos[1] - z1.y)):
-        z1.x = z1.init_x
-        z1.y = z1.init_y
+    if not z1_dead:
 
-    if z1_mask.overlap(left_mask, (350 - z1.x, 385 - z1.y)):
-        z1.x -= 100
-        health -= 10
-    if z1_mask.overlap(right_mask, (420 - z1.x, 385 - z1.y)):
-        z1.x += 100
-        health -= 10
-    if z1_mask.overlap(top_mask, (385 - z1.x, 350 - z1.y)):
-        z1.y -= 100
-        health -= 10
-    if z1_mask.overlap(bottom_mask, (385 - z1.x, 420 - z1.y)):
-        z1.y += 100
-        health -= 10
+        z1_action = z1.move_towards_player(p)
 
-    if z2_mask.overlap(walls_mask, (pos[0] - z2.x, pos[1] - z2.y)):
-        z2.x = z2.init_x
-        z2.y = z2.init_y
-    if z2_mask.overlap(left_mask, (350 - z2.x, 385 - z2.y)):
-        z2.x -= 100
-        health -= 10
-    if z2_mask.overlap(right_mask, (420 - z2.x, 385 - z2.y)):
-        z2.x += 100
-        health -= 10
-    if z2_mask.overlap(top_mask, (385 - z2.x, 350 - z2.y)):
-        z2.x -= 100
-        health -= 10
-    if z2_mask.overlap(bottom_mask, (385 - z2.x, 420 - z2.y)):
-        z2.y += 100
-        health -= 10
+        if z1_mask.overlap(walls_mask, (pos[0] - z1.x, pos[1] - z1.y)):
+            z1.x = z1.init_x
+            z1.y = z1.init_y
+        if z1_mask.overlap(left_mask, (350 - z1.x, 385 - z1.y)):
+            z1.x -= 100
+            health -= 10
+        elif z1_mask.overlap(right_mask, (420 - z1.x, 385 - z1.y)):
+            z1.x += 100
+            health -= 10
+        elif z1_mask.overlap(top_mask, (385 - z1.x, 350 - z1.y)):
+            z1.y -= 100
+            health -= 10
+        elif z1_mask.overlap(bottom_mask, (385 - z1.x, 420 - z1.y)):
+            z1.y += 100
+            health -= 10
+
+    if not z2_dead:
+
+        z2_action = z2.move_towards_player(p)
+
+        if z2_mask.overlap(walls_mask, (pos[0] - z2.x, pos[1] - z2.y)):
+            z2.x = z2.init_x
+            z2.y = z2.init_y
+        if z2_mask.overlap(left_mask, (350 - z2.x, 385 - z2.y)):
+            z2.x -= 100
+            health -= 10
+        elif z2_mask.overlap(right_mask, (420 - z2.x, 385 - z2.y)):
+            z2.x += 100
+            health -= 10
+        elif z2_mask.overlap(top_mask, (385 - z2.x, 350 - z2.y)):
+            z2.x -= 100
+            health -= 10
+        elif z2_mask.overlap(bottom_mask, (385 - z2.x, 420 - z2.y)):
+            z2.y += 100
+            health -= 10
+
+    if z1_dead:
+        current_time = pygame.time.get_ticks()
+
+        if current_time - z1_died_time >= 5000:
+            z1_dead = False
+            z1_health = 100
+            z1.x, z1.y = z1.init_x, z1.init_y 
+
+    if z2_dead:
+        current_time = pygame.time.get_ticks()
+
+        if current_time - z2_died_time >= 5000:
+            z2_dead = False
+            z2_health = 100
+            z2.x, z2.y = z2.init_x, z2.init_y
 
     # --- Main event loop
     for event in pygame.event.get():  
@@ -223,26 +247,58 @@ while run:
                 print("left")
                 if sword_mask.overlap(z1_mask, (z1.x - 315, z1.y - 385)):
                     print("hit")
-                if sword_mask.overlap(z1_mask, (z2.x - 315, z2.y - 385)):
+                    z1_health -= 50
+                    if z1_health <= 0:
+                        z1_dead = True
+                        z1_died_time = pygame.time.get_ticks()
+                if sword_mask.overlap(z2_mask, (z2.x - 315, z2.y - 385)):
                     print("hit")
+                    z2_health -= 50
+                    if z2_health <= 0:
+                        z2_dead = True
+                        z2_died_time = pygame.time.get_ticks()
             elif temp_x > 0 and temp_x >= abs(temp_y):
                 print("right")
                 if sword_mask.overlap(z1_mask, (z1.x - 445, z1.y - 385)):
                     print("hit")
+                    z1_health -= 50
+                    if z1_health <= 0:
+                        z1_dead = True
+                        z1_died_time = pygame.time.get_ticks()
                 if sword_mask.overlap(z2_mask, (z2.x - 445, z2.y - 385)):
                     print("hit")
+                    z2_health -= 50
+                    if z2_health <= 0:
+                        z2_dead = True
+                        z2_died_time = pygame.time.get_ticks()
             elif temp_y > 0:
                 print("down")
                 if sword_mask.overlap(z1_mask, (z1.x - 385, z1.y - 445)):
                     print("hit")
+                    z1_health -= 50
+                    if z1_health <= 0:
+                        z1_dead = True
+                        z1_died_time = pygame.time.get_ticks()
                 if sword_mask.overlap(z2_mask, (z2.x - 385, z2.y - 445)):
                     print("hit")
+                    z2_health -= 50
+                    if z2_health <= 0:
+                        z2_dead = True
+                        z2_died_time = pygame.time.get_ticks()
             elif temp_y < 0:
                 print("up")
                 if sword_mask.overlap(z1_mask, (z1.x - 385, z1.y - 315)):
                     print("hit")
+                    z1_health -= 50
+                    if z1_health <= 0:
+                        z1_dead = True
+                        z1_died_time = pygame.time.get_ticks()
                 if sword_mask.overlap(z2_mask, (z2.x - 385, z2.y - 315)):
                     print("hit")
+                    z2_health -= 50
+                    if z2_health <= 0:
+                        z2_dead = True
+                        z2_died_time = pygame.time.get_ticks()
 
     # Blit
     screen.fill((r, g, b))
@@ -252,19 +308,21 @@ while run:
     # Animation
     current_time = pygame.time.get_ticks()
 
-    if current_time - last_update >= animation_cooldown:
-        frame += 1
-        last_update = current_time
-        if frame >= len(zombie_list[z1_action]):
-            frame = 0
-    screen.blit(zombie_list[z1_action][frame], (z1.x, z1.y))
+    if not z1_dead:
+        if current_time - last_update >= animation_cooldown:
+            frame += 1
+            last_update = current_time
+            if frame >= len(zombie_list[z1_action]):
+                frame = 0
+        screen.blit(zombie_list[z1_action][frame], (z1.x, z1.y))
 
-    if current_time - last_update >= animation_cooldown:
-        frame += 1
-        last_update = current_time
-        if frame >= len(zombie_list[z2_action]):
-            frame = 0
-    screen.blit(zombie_list[z2_action][frame], (z2.x, z2.y))
+    if not z2_dead:
+        if current_time - last_update >= animation_cooldown:
+            frame += 1
+            last_update = current_time
+            if frame >= len(zombie_list[z2_action]):
+                frame = 0
+        screen.blit(zombie_list[z2_action][frame], (z2.x, z2.y))
 
     if current_time - last_update >= animation_cooldown:
         frame += 1
